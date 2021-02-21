706a986601a13a828edc4e041e92770b277d04ea
dlasecki <dario.lasecki@gmail.com>
2021-02-21 12:23:22 +0100

    KDE models provider implemented.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-import dill
-
 from flask import Flask
 from flask_restx import Api, reqparse, Resource
+
+from exceptions.kde_model_not_found_exception import KdeModelNotFoundException
+from kde_models_provider import get_kde_model
 
 app = Flask(__name__)
 
@@ -20,6 +21,26 @@ class KdeModels(Resource):
     @ns.doc('Get predicted parameters.')
     def post(self):
         """Returns parameters sampled from the KDE model base on arguments provided."""
+
+        problem_name, graph_type, p_depth, num_samples = self._parse_arguments()
+
+        directory = "/serialized_models/"
+
+        try:
+            relevant_kde_model = get_kde_model(directory, problem_name, graph_type, p_depth)
+        except KdeModelNotFoundException as err:
+            return 'KDE model with given parameters not found.', 400
+
+        sampled_params = []
+
+        for _ in range(num_samples):
+            sampled_params.append(relevant_kde_model.kde_model.sample().tolist())
+
+        # add the newly provided values
+
+        return {'sampled_params': sampled_params}, 200  # return data with 200 OK
+
+    def _parse_arguments(self):
         parser = reqparse.RequestParser()  # initialize
         parser.add_argument('problem_name', type=str, required=True)  # add args
         parser.add_argument('graph_type', type=str, required=True)
@@ -28,22 +49,12 @@ class KdeModels(Resource):
 
         args = parser.parse_args()  # parse arguments to dictionary
 
-        example_model_path = "serialized_models/kde_model_max_cut_random_gaussian_bandwidth=0.2_p=1"
-        example_model = dill.load(open(example_model_path, 'rb'))
-
         problem_name = args['problem_name']
         graph_type = args['graph_type']
         p_depth = args['p_depth']
         num_samples = args['num_samples']
 
-        sampled_params = []
-
-        for _ in range(num_samples):
-            sampled_params.append(example_model.kde_model.sample().tolist())
-
-        # add the newly provided values
-
-        return {'sampled_params': sampled_params}, 200  # return data with 200 OK
+        return problem_name, graph_type, p_depth, num_samples
 
 
 if __name__ == '__main__':
---
+++ exceptions/kde_model_not_found_exception.py
@@ -0,0 +1,2 @@
+class KdeModelNotFoundException(Exception):
+    pass
