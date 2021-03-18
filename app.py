13319f72b9fa246afff2753531aea0cb38bfd826
dlasecki <dario.lasecki@gmail.com>
2021-03-18 21:48:07 +0100

    ProxyFix

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import werkzeug
 from flask import Flask
 from flask_restx import Api
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 from kde_resource import ns as kde_ns
 
 werkzeug.cached_property = werkzeug.utils.cached_property  # it fixes an issue with werkzeug
 
 app = Flask(__name__)
-
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
 api = Api(app, title='QAOA params predictor API', description='API for predicting QAOA params.')
 api.add_namespace(kde_ns)
 
 if __name__ == '__main__':
-    app.run()  # run our Flask app
+    app.run(host='0.0.0.0')
